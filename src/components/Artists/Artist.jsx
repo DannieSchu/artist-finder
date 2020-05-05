d05dd90a0ab64a82c74653403efbeec1d70d78f4
Dannie Schumaker <draeschumaker@gmail.com>
2020-05-04 22:29:25 -0700

    style table in each Artist component

--- src/components/Artists/Artist.jsx
+++ src/components/Artists/Artist.jsx
@@ -1,30 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from './Artist.css';
 
 const Artist = ({ name, origin, birth, death }) => {
   return (
-    <>
-      <tr>
-        <th>Artist</th>
-      </tr>
-      <tr>
-        <td>{name}</td>
-      </tr>
-      <tr>
-        {origin && <th>Origin</th>}
-      </tr>
-      <tr>
-        {origin && <td>{origin}</td>}
-      </tr>
-      <tr>
-        {birth && <th>Birth</th>}
-        {death && <th>Death</th>}
-      </tr>
-      <tr>
-        {birth && <td>{birth.slice(0, 4)}</td>}
-        {death && <td>{death.slice(0, 4)}</td>}
-      </tr>
-    </>
+    <table className={styles.Artist}>
+      <>
+        <tr>
+          <th>Artist</th>
+          <td>{name}</td>
+        </tr>
+        <tr>
+          {origin && <td>{origin}</td>}
+          {origin && <th>Origin</th>}
+        </tr>
+        <tr>
+          {birth && <th>DOB</th>}
+          {birth && <td>{birth.slice(0, 4)}</td>}
+        </tr>
+        <tr>
+          {death && <td>{death.slice(0, 4)}</td>}
+          {death && <th>Death</th>}
+        </tr>
+      </>
+    </table>
   );
 };
 
@@ -56,3 +55,24 @@ export default Artist;
   {death && <td>{death.slice(0, 4)}</td>}
 </tr>
 </> */}
+
+{/* <tr>
+        <th>Artist</th>
+      </tr>
+      <tr>
+        <td>{name}</td>
+      </tr>
+      <tr>
+        {origin && <th>Origin</th>}
+      </tr>
+      <tr>
+        {origin && <td>{origin}</td>}
+      </tr>
+      <tr>
+        {birth && <th>Birth</th>}
+        {death && <th>Death</th>}
+      </tr>
+      <tr>
+        {birth && <td>{birth.slice(0, 4)}</td>}
+        {death && <td>{death.slice(0, 4)}</td>}
+      </tr> */}
